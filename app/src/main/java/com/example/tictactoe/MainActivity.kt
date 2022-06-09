e5a56c4304820ebf8f2cdc78fe9b8ddfbb076484
15k61a0503 <15k61a0503@gmail.com>
2022-06-09 03:37:42 -0500

    Tik Tac To

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.example.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val boardCells = Array(3) { arrayOfNulls<ImageView>(3) }
+    private lateinit var layout_board: GridLayout
+    private lateinit var button_restart: Button
+    private lateinit var text_view_result: TextView
+
+    var board = Board()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        layout_board = findViewById(R.id.layout_board)
+        button_restart = findViewById(R.id.button_restart)
+        text_view_result = findViewById(R.id.text_view_result)
+        loadBoard()
+
+        //restart functionality
+        button_restart.setOnClickListener {
+            //creating a new board instance
+            //it will empty every cell
+            board = Board()
+
+            //setting the result to empty
+            text_view_result.text = ""
+
+            //this function will map the internal board
+            //to the visual board
+            mapBoardToUi()
+        }
+    }
+
+    //function is mapping
+    //the internal board to the ImageView array board
+    private fun mapBoardToUi() {
+        for (i in board.board.indices) {
+            for (j in board.board.indices) {
+                when (board.board[i][j]) {
+                    Board.PLAYER -> {
+                        boardCells[i][j]?.setImageResource(R.drawable.circle)
+                        boardCells[i][j]?.isEnabled = false
+                    }
+                    Board.COMPUTER -> {
+                        boardCells[i][j]?.setImageResource(R.drawable.cross)
+                        boardCells[i][j]?.isEnabled = false
+                    }
+                    else -> {
+                        boardCells[i][j]?.setImageResource(0)
+                        boardCells[i][j]?.isEnabled = true
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadBoard() {
+
+        for (i in boardCells.indices) {
+            for (j in boardCells.indices) {
+                boardCells[i][j] = ImageView(this)
+                boardCells[i][j]?.layoutParams = GridLayout.LayoutParams().apply {
+                    rowSpec = GridLayout.spec(i)
+                    columnSpec = GridLayout.spec(j)
+                    width = 250
+                    height = 230
+                    bottomMargin = 5
+                    topMargin = 5
+                    leftMargin = 5
+                    rightMargin = 5
+                }
+                boardCells[i][j]?.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
+                boardCells[i][j]?.setOnClickListener(CellClickListener(i, j))
+                layout_board.addView(boardCells[i][j])
+            }
+        }
+    }
+
+    inner class CellClickListener(
+        val i: Int,
+        val j: Int
+    ) : View.OnClickListener {
+
+        override fun onClick(p0: View?) {
+            //checking if the game is not over
+            if (!board.isGameOver) {
+
+                //creating a new cell with the clicked index
+                val cell = Cell(i, j)
+
+                //placing the move for player
+                board.placeMove(cell, Board.PLAYER)
+
+                //calling minimax to calculate the computers move
+                board.minimax(0, Board.COMPUTER)
+
+                //performing the move for computer
+                board.computersMove?.let {
+                    board.placeMove(it, Board.COMPUTER)
+                }
+
+                //mapping the internal board to visual board
+                mapBoardToUi()
+            }
+
+            //Displaying the results
+            //according to the game status
+            when {
+                board.hasComputerWon() -> text_view_result.text = "Computer Won"
+                board.hasPlayerWon() -> text_view_result.text = "Player Won"
+                board.isGameOver -> text_view_result.text = "Game Tied"
+            }
+        }
+
+    }
+}
+
